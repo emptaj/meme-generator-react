69f3dcd91980c7e28c088560af5a48b3cb500425
Marcin Szyszka <toja@example.com>
2022-04-13 21:54:48 +0200

    Added generate button

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,13 +1,14 @@
+import { useState } from "react";
 
 const MemeGenerator = () => {
     return (
         <div className="meme-generator">
             <form className="form-generator">
-                <input type="text" name="first-line" defaultValue="Shut up" />
+                <input type="text" name="first-line"
+                    defaultValue="Shut up" />
                 <input type="text" name="second-line" defaultValue="and take my money" />
-
-
             </form>
+            <button className="meme-generate-button">Get a new image</button>
         </div>
     );
 }
