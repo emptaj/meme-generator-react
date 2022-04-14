e4a5d6b84739a80caa066d2611ead91a79331f0f
Marcin Szyszka <toja@example.com>
2022-04-14 11:53:38 +0200

    Added loading image from external file

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,6 +1,23 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import memesData
+    from "./utils/memesData";
 const MemeGenerator = () => {
+    const [imageUrl, setImageUrl] = useState(null);
+    const [imageWidth, setImageWidth] = useState(null);
+    const [imageHeight, setImageHeight] = useState(null);
+
+
+    const getRandomImageUrl = () => {
+        const urls = memesData.data.memes
+        const randIndex = Math.floor(Math.random() * urls.length)
+        const randomUrl = urls[randIndex]
+        setImageWidth(randomUrl.width)
+        setImageHeight(randomUrl.height)
+        setImageUrl(randomUrl.url)
+    }
+
+    useEffect(getRandomImageUrl, [])
+
     return (
         <div className="meme-generator">
             <form className="form-generator">
@@ -8,7 +25,14 @@ const MemeGenerator = () => {
                     defaultValue="Shut up" />
                 <input type="text" name="second-line" defaultValue="and take my money" />
             </form>
-            <button className="meme-generate-button">Get a new image</button>
+            <img className="meme-image"
+                style={{
+                    width: `${imageWidth}px`,
+                    height: `${imageHeight}px`
+                }}
+                src={imageUrl} />
+            <button className="meme-generate-button"
+                onClick={getRandomImageUrl}>Get a new image</button>
         </div>
     );
 }
