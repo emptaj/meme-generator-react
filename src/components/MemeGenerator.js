07953879df1d09416b16cff262d398ed10522b1e
Marcin Szyszka <toja@example.com>
2022-04-22 17:33:08 +0200

    Fetching memes from API

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,20 +1,37 @@
 import { useState, useEffect } from "react";
 import MemeForm from "./MemeForm";
-import memesData
-    from "./utils/memesData";
+import memesData from "./utils/memesData";
 
 
 const MemeGenerator = () => {
-    const [imageObject, setImageObject] = useState({});
+    const API_LINK = "https://api.imgflip.com/get_memes"
+
+    const [memes, setMemes] = useState([]);
+    const [imageObject, setImageObject] = useState({
+        url: "http://i.imgflip.com/1bij.jpg"
+    });
     const [memeData, setMeme] = useState({
         firstLine: "",
         secondLine: ""
     })
 
 
+    useEffect(() => {
+        async function getMemes() {
+            const response = await fetch(API_LINK).catch(error => setMemes(memesData.data.memes))
+            const data = await response.json()
+            setMemes(data.data.memes)
+        }
+
+        getMemes()
+    }, [])
+
+    useEffect(() => getRandomImage(), [memes])
+
+
     const getRandomImage = () => {
-        const randIndex = Math.floor(Math.random() * memesData.data.memes.length)
-        const randObject = memesData.data.memes[randIndex]
+        const randIndex = Math.floor(Math.random() * memes.length)
+        const randObject = memes[randIndex]
 
         setImageObject(randObject)
     }
@@ -32,21 +49,20 @@ const MemeGenerator = () => {
 
     }
 
-    useEffect(getRandomImage, [])
-
     return (
         <div className="meme-generator">
             <MemeForm
                 firstLineValue={memeData.firstLine}
                 secondLineValue={memeData.secondLine}
                 onChangeFunc={handleFormChange} />
-            <img className="meme-image"
+            {imageObject && <img className="meme-image"
                 style={{
                     width: `${imageObject.width}px`,
                     height: `${imageObject.height}px`
                 }}
                 src={imageObject.url}
                 alt="oups.." />
+            }
             <button className="meme-generate-button"
                 onClick={getRandomImage}>Get a new image</button>
         </div>
